c29310e5eee51a753980ae6fb4432fa43728b9b7
kleber <kleber107@gmail.com>
2021-03-13 19:43:19 -0300

    change url query - Regular expression operations

--- tmp.py
+++
@@ -1,6 +0,0 @@
-url = 'https://stackoverflow.com/questions/3437059/does-python-have-a-string-contains-substring-method'
-print url
-if "http" in url: 
-    print "tem http ou https"
-else:
-    print "nao tem http ou https"
